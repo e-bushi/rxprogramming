ba08f62c4b1c4fbae046a21e4a4113310f935c95
Chris Mauldin <Chrislovemauldin@gmail.com>
2018-01-12 05:57:52 -0800

    Completed example of behavior subjects

--- 3_subjects/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ 3_subjects/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -29,45 +29,83 @@ PlaygroundPage.current.needsIndefiniteExecution = true
  */
 
 
-example(of: "PublishSubject") {
-    let subject = PublishSubject<String>()
+//example(of: "PublishSubject") {
+//    let subject = PublishSubject<String>()
+//
+//    subject.onNext("Is anyone listening?")
+//
+//    let subscriptionOne = subject.subscribe(onNext: { string in
+//        print(string)
+//    })
+//
+//    subject.on(.next("1"))
+//
+//    subject.onNext("2")
+//
+//    let subscriptionTwo = subject
+//        .subscribe { event in
+//            print("2)", event.element ?? event)
+//    }
+//
+//    subject.onNext("3")
+//
+////    subscriptionOne.dispose()
+//
+//    subject.onNext("4")
+//
+//    subject.onCompleted()
+//
+//    subject.onNext("5")
+//
+//    subscriptionTwo.dispose()
+//
+//    let disposeBag = DisposeBag()
+//
+//    subject
+//        .subscribe {
+//            print("3)", $0.element ?? $0)
+//            }
+//    .disposed(by: disposeBag)
+//
+//
+//    subject.onNext("?")
+//
+//}
+
+enum MyError: Error {
+    case anError
+}
+
+func print<T: CustomStringConvertible> (label: String, event: Event<T>) {
+    print(label, event.element ?? event.error ?? event)
+}
+
+/*
+ Since BehaviorSubject always emits the latest element, you can’t create one without
+ providing a default initial value. If you can’t provide a default initial value at
+ creation time, that probably means you need to use a PublishSubject instead.”
+*/
+
+example(of: "BehaviorSubject") {
     
-    subject.onNext("Is anyone listening?")
-    
-    let subscriptionOne = subject.subscribe(onNext: { string in
-        print(string)
-    })
-    
-    subject.on(.next("1"))
-    
-    subject.onNext("2")
-    
-    let subscriptionTwo = subject
-        .subscribe { event in
-            print("2)", event.element ?? event)
-    }
-    
-    subject.onNext("3")
-    
-    subscriptionOne.dispose()
-    
-    subject.onNext("4")
-    
-    subject.onCompleted()
-    
-    subject.onNext("5")
-    
-    subscriptionTwo.dispose()
+    let subject = BehaviorSubject(value: "Initial Value")
     
     let disposeBag = DisposeBag()
     
+    subject.onNext("X")
+    
     subject
         .subscribe {
-            print("3)", $0.element ?? $0)
-            }
+         print(label: "1)", event: $0)
+    }
     .disposed(by: disposeBag)
     
+    subject.onError(MyError.anError)
     
-    subject.onNext("?")
+    subject
+        .subscribe {
+        print(label: "2)", event: $0)
+    }
+    .disposed(by: disposeBag)
     
 }
